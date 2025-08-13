b06ec48de4f9c29dfc4a5f24bfbe9cbca9c49faa
petripetcare-cloud <petripetcare@gmail.com>
2025-08-12 17:04:19 -0700

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  // CORS (helps browsers/tools)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.status(200).end();
+  res.status(200).json({ ok: true, route: "/api/ping", method: req.method });
+}
